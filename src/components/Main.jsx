9a501d6bf75bf0a70cf697915c71fdd8e439ee67
Shryler <105290336+Shryler@users.noreply.github.com>
2022-08-23 13:20:17 +0200

    maj

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,19 @@
+import MyCard from "./MyCard";
 import "./styles/main.css";
 
 function Main() {
     return (
-        <main className="bg-danger container vh-100">Main</main>
+        <>
+        <main className="bg-primary container">
+        <div className="imgMyCard">
+            <MyCard imageSrc="https://picsum.photos/id/237/200/300" cardTitle="Titre 1" />
+            <MyCard imageSrc="https://picsum.photos/id/238/200/300" cardTitle="Titre 2" />
+            <MyCard imageSrc="https://picsum.photos/id/239/200/300" cardTitle="Titre 3" />
+            <MyCard imageSrc="https://picsum.photos/id/240/200/300" cardTitle="Titre 4" />
+            <MyCard imageSrc="https://picsum.photos/id/241/200/300" cardTitle="Titre 5" />
+        </div>
+        </main>
+        </>
     );
 }
 
---
+++ src/components/MyCard.jsx
@@ -0,0 +1,18 @@
+
+
+function MyCard(props) {
+    return (
+        <>
+            <div className="card">
+                <img src={props.imageSrc} className="card-img-top" alt="Images" />
+                <div className="card-body">
+                    <h5 className="card-title">{props.cardTitle}</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="/" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default MyCard;
